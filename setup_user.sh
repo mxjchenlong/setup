63e3da055821c04bce888b04a2c7c73f47b37694
Bill Lee <admin@minereu.com>
2015-07-26 16:59:20 +0800

    initial commit

---
+++ setup_user.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+userdel bill
+useradd bill -u 5001  -m -s /bin/bash
+touch /etc/sudoers.d/bill
+tee /etc/sudoers.d/bill 1>/dev/null <<SUDOFILE
+bill ALL=(ALL) NOPASSWD:ALL
+SUDOFILE
+chmod 0440 /etc/sudoers.d/bill
+chown -R  bill:bill /home/bill
+mkdir -p touch /home/bill/.ssh/
+touch /home/bill/.ssh/authorized_keys
+touch /home/bill/.ssh/config
+tee /home/bill/.ssh/authorized_keys 1>/dev/null <<AUTHPUBKEY
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCms4MdgAujbVFgq+nc8zjovCo3Ccy8x697D6T3Ximl0868k1hzIvp3DQe6FwssxE9qTkWv0y1x0YvpIudsVGky1IPQY4/LcDe61W9ZzUAs/piydvUwo4mKsSpqjYkCidaiE7JuP1Y64ay9Quvp+mmZIM0QVNA8tXOeCmZ1B5m1bkbQRqgKmT3T47jHPfpPUbDjXFNHc7JpRuJ/dU1gK16zqhOm1VapMEfT3nA69fIc84i2bem2cHqwsxqjjL2TsuqNAX7aj0sVdTvweZKq/5iYT5cZzGZidqU6RDCr1JeTbb1Gtd4mAjvzQpzrERgyujn54I9C6/sCavb9GdEuDfQ/ bill@Bills-MacBook-Pro.local
+AUTHPUBKEY
+echo  "bill:f1zzb4ck" | chpasswd
+tee /home/bill/.ssh/config 1>/dev/null <<SSHCONFIG
+Host *
+   StrictHostKeyChecking no
+   UserKnownHostsFile=/dev/null
+SSHCONFIG
+chown -R bill:bill /home/bill/
+chmod -R og-rw /home/bill/.ssh
